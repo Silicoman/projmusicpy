d4521f4a03e616527bf4a07add5810c412817660
BOURGUIGNON <louis.bourguignon@capgemini.com>
2018-01-30 13:12:25 +0100

    metadataToObject ok

--- ToolMusicFile/ModelTrack.py
+++ ToolMusicFile/ModelTrack.py
@@ -1,23 +1,54 @@
-'''
-Created on 29 janv. 2018
 
-@author: louisbourguignon
-'''
-
-class MyClass(object):
+class ModelTrack(object):
     '''
-    classdocs
+    Model to object Track with a dictionnary of metadata values:
+    -album;    -bpm;    -title
+    -artist    -tracknumber;
+    -genre;    -date;
     '''
-    album;
-    bpm;
-    title
-    artist
-    tracknumber;
-    genre;
-    date;
+    objectID=0
     
-    def __init__(self, params):
+    def __init__(self, nameFile,dicAtt,mimeArg):
         '''
-        Constructor
+        Constructor 
+        +1 ID for each new track
         '''
-        
+        self.objectID+=1
+        self.setNameFile(nameFile)
+        self.setDicAttTrack(dicAtt)
+        self.setMimeTypeList(mimeArg)
+
+
+    # SETTER & GETTER & DOCSTRING
+    def getMimeTypeList(self):
+        return self.__mimeTypeList
+
+
+    def setMimeTypeList(self, value):
+        self.__mimeTypeList = value
+
+
+    def getNameFile(self):
+        return self.__nameFile
+
+    def setNameFile(self, value):
+        self.__nameFile = value
+
+    # properties DicAtt == metadata track
+    def getDicAttTrack(self):
+        return self.__dicAttTrack
+
+    def setDicAttTrack(self, value):
+        self.__dicAttTrack = value
+
+    def delDicAttTrack(self):
+        del self.__dicAttTrack
+
+    
+    dicAttTrack = property(getDicAttTrack, setDicAttTrack, delDicAttTrack, 
+                           "dicAttTrack's docstring: dictionnary attribut metadata track")
+    nameFile = property(getNameFile, setNameFile, None,
+                        "nameFile's docstring: origin name file")
+    mimeTypeList = property(getMimeTypeList, setMimeTypeList, None, 
+                        "mimeTypeList's docstring: list mime File")
+    
--- ToolMusicFile/ReadAndTransform.py
+++ ToolMusicFile/ReadAndTransform.py
@@ -1,32 +1,47 @@
+import os
 from mutagen.easyid3 import EasyID3
 from mutagen.mp3 import MP3
 import mutagen 
-import magic
+#import magic
+from ToolMusicFile.ModelTrack import ModelTrack
 
-if __name__ == '__main__':
-   
+
+def DefineMetadataFromMime(mutagenFile):
+    ''' define metadata , depending of Mime Type'''
+    dictionnaryMetadata={}
+    typeMime=mutagenFile.mime    
+    # CASE MP3
+    if ('audio/mp3' in typeMime):
+        try :
+            dictionnaryMetadata=MP3(myFileToAnalyze, ID3=EasyID3)
+        except :
+            print("problem to case MP3") 
+    # CASE FLAC
+    # CASE OGG
+    else :
+        print("Mime file not taken")
+    return dictionnaryMetadata;
+
+def ReadTrackFileAndTransform(pathToTrack):
+    myFileName=pathToTrack.split('/')[-1]
     
-    myFileToAnalyze="../Music/06 What You Know About Richard D. James.mp3";
-
-    # need try catch
-    openFile=mutagen.File(myFileToAnalyze)
-    if (openFile != None):
-              
-        #define type of file
-        #MyFileSplit=myFileToAnalyze.split('.')
-        #extFile=""+MyFileSplit.__getitem__(MyFileSplit.__len__()-1)
-        #extFile=extFile.lower()
-        mime = magic.Magic(mime=True)
-        typeMime=mime.from_file(myFileToAnalyze).split('/')[-1]
-        print(typeMime)
-        
-        if (typeMime=='mpeg'):
-            
-            print(True)
-            audio = MP3(myFileToAnalyze, ID3=EasyID3)
-            print(audio)       
-            
-            print(audio.keys())
+    openFile=mutagen.File(pathToTrack)
+    if (openFile != None): 
+        typeMime=openFile.mime
+        dictioMetadata=DefineMetadataFromMime(openFile)
+        track=ModelTrack(myFileName,dictioMetadata,typeMime)
+        return track
     else :
         print("FILE impossible to read by mutagen")
+
+
+if __name__ == '__main__':
+    os.chdir("../music/")
+    
+    myFileToAnalyze="06 What You Know About Richard D. James.mp3";
+    
+    myTrack=ReadTrackFileAndTransform(myFileToAnalyze)
+    
+    print(myTrack.dicAttTrack)
+
     pass
